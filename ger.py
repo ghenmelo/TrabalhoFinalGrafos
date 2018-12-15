f2cdf47016e48361458b1ffc6ffb2e06b72dbe23
gbochikubo <guilhermeochikubo@gmail.com>
2018-12-15 15:13:21 -0200

    Continuando adaptaçao para aceitar grafo

--- ger.py
+++ ger.py
@@ -1,6 +1,7 @@
 import networkx as nx
 
 G = nx.Graph()
+G.add_node(1,Silvia=69)
 G.add_nodes_from([1,2,3,4,5],visitado = False,dinheiro = 0,volume = 0)
 G.add_edges_from([(1,2),(1,3),(1,4),(1,5),(2,3),(2,4),(2,5),(3,4),(3,5),(4,5)],caminho = False)
 G[1][2]["distancia"] = 2
@@ -26,5 +27,5 @@ G.node[3]["volume"] = 30
 G.node[4]["volume"] = 15
 G.node[5]["volume"] = 5
 
-a = {1,2,3,4}
+a = {}
 print (a)
